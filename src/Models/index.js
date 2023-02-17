099bf3192715aef5050226fd707f17521651ff1c
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-02-17 13:10:07 +0000

    add User/Board association and test

--- src/Models/index.js
+++ src/Models/index.js
@@ -2,6 +2,9 @@ const { User } = require("./User");
 const { Board } = require("./Board");
 const { Cheese } = require("./Cheese");
 
+User.hasMany(Board);
+Board.belongsTo(User);
+
 module.exports = {
   User,
   Board,
--- src/index.test.js
+++ src/index.test.js
@@ -8,9 +8,11 @@ describe("User, Board and Cheese Models", () => {
 
     user1 = await User.create(seedUsers[0]);
     board1 = await Board.create(seedBoards[0]);
+    board2 = await Board.create(seedBoards[0]);
     cheddar = await Cheese.create(seedCheeses[0]);
   });
 
+  // Create, Find, Delete Model data
   it("can create a User", async () => {
     expect(user1).toBeInstanceOf(User);
     expect(user1.name).toEqual("user1");
@@ -33,7 +35,7 @@ describe("User, Board and Cheese Models", () => {
 
     expect(users.length).toEqual(1);
     expect(users[0].name).toEqual("user1");
-    expect(boards.length).toEqual(1);
+    expect(boards.length).toEqual(2);
     expect(boards[0].type).toEqual("board1");
     expect(cheeses.length).toEqual(1);
     expect(cheeses[0].title).toEqual("Cheddar");
@@ -59,7 +61,7 @@ describe("User, Board and Cheese Models", () => {
 
     await boardDelete.destroy();
     const boards = await Board.findAll();
-    expect(boards.length).toEqual(1);
+    expect(boards.length).toEqual(2);
   });
 
   it("can delete from Cheese", async () => {
@@ -71,5 +73,13 @@ describe("User, Board and Cheese Models", () => {
     await cheeseDelete.destroy();
     const cheeses = await Cheese.findAll();
     expect(cheeses.length).toEqual(1);
+  });
+
+  // Model association
+  it("can add a Boards to a User", async () => {
+    await user1.addBoard([board1, board2]);
+    const user1Boards = await user1.getBoards();
+
+    expect(user1Boards.length).toEqual(2);
   });
 });
